30395d34dba622f4e59116381894ad20a08ceaf8
Johnkai Cortez <73104201+Johnkai2196@users.noreply.github.com>
2022-09-08 22:11:53 +0300

    work in proggress

--- app/src/main/java/com/example/bluetooth/MainActivity.kt
+++ app/src/main/java/com/example/bluetooth/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.bluetooth
 
 import android.Manifest
+import android.app.Activity
+import android.app.Application
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
 import android.bluetooth.le.BluetoothLeScanner
@@ -8,13 +10,19 @@ import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
 import android.bluetooth.le.ScanSettings
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
@@ -24,11 +32,16 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.bluetooth.MainActivity.GattAttributes.SCAN_PERIOD
 import com.example.bluetooth.ui.theme.BluetoothTheme
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import java.util.*
 
 class MainActivity : ComponentActivity() {
@@ -41,7 +54,9 @@ class MainActivity : ComponentActivity() {
         const val STATE_CONNECTED = 2
         val UUID_HEART_RATE_MEASUREMENT = UUID.fromString("00002a37-0000-1000-8000-00805f9b34fb")
         val UUID_HEART_RATE_SERVICE = UUID.fromString("0000180d-0000-1000-8000-00805f9b34fb")
-        val UUID_CLIENT_CHARACTERISTIC_CONFIG = UUID.fromString("00002902-0000-1000-8000-00805f9b34fb")
+        val UUID_CLIENT_CHARACTERISTIC_CONFIG =
+            UUID.fromString("00002902-0000-1000-8000-00805f9b34fb")
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,14 +64,23 @@ class MainActivity : ComponentActivity() {
         setContent {
             val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
             mBluetoothAdapter = bluetoothManager.adapter
-            val mResults = java.util.HashMap<String, ScanResult>()
+            val mResults = HashMap<String, ScanResult>()
             BluetoothTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    if (hasPermissions()) {
+                    Column() {
+                        val model: MyViewModel by viewModels()
+                        Button(onClick = {
+                            model.scanDevices(
+                                bluetoothManager.adapter.bluetoothLeScanner,
+                            )
+                        }) {
+                            Text(text = "gay")
+                        }
+
 
                     }
                 }
@@ -79,32 +103,66 @@ class MainActivity : ComponentActivity() {
 
 }
 
-class MyViewModel : ViewModel() {
+class MyViewModel(application: Application) : AndroidViewModel(application) {
     val scanResults = MutableLiveData<List<ScanResult>>(null)
     val fScanning = MutableLiveData<Boolean>(false)
-    private val mResults = java.util.HashMap<String, ScanResult>()
+    private val mResults = HashMap<String, ScanResult>()
+    private val context = getApplication<Application>()
 
+    fun scanDevices(scanner: BluetoothLeScanner) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            if (ActivityCompat.checkSelfPermission(
+                    context,
+                    Manifest.permission.BLUETOOTH_SCAN
+                ) != PackageManager.PERMISSION_GRANTED
+            ) {
+                ActivityCompat.requestPermissions(
+                    Activity(),
+                    arrayOf(Manifest.permission.BLUETOOTH_SCAN), 1
+                )
 
-    //scanResults.postValue(mResults.values.toList())
-    private val leScanCallback: ScanCallback = object : ScanCallback() {}
+            }
+        }
+        viewModelScope.launch(Dispatchers.IO) {
+            fScanning.postValue(true)
+            val settings = ScanSettings.Builder()
+                .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+                .setReportDelay(0)
+                .build()
 
+            scanner.startScan(null, settings, leScanCallback)
+            delay(SCAN_PERIOD)
+            scanner.stopScan(leScanCallback)
+            scanResults.postValue(mResults.values.toList())
+            fScanning.postValue(false)
+        }
+    }
+
+    private val leScanCallback: ScanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            super.onScanResult(callbackType, result)
+            val device = result.device
+            val deviceAddress = device.address
+            mResults[deviceAddress] = result
+            Log.i("DBG", "Device address: $deviceAddress (${result.isConnectable})")
+        }
+    }
 }
 
+/*
 @Composable
 fun ShowDevices(mBluetoothAdapter: BluetoothAdapter, model: MyViewModel = MyViewModel()) {
     val context = LocalContext.current
-    val value: List<ScanResult>? by model.scanResults.observeAsState(null)
+    val values: List<ScanResult>? by model.scanResults.observeAsState(null)
     val fScanning: Boolean by model.fScanning.observeAsState(false)
     Column {
-
-    }
-
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    BluetoothTheme {
-
+        LazyColumn {
+            values?.size?.let {
+                items(it) {
+                    Log.i("DBG", "dbg")
+                }
+            }
+        }
     }
 }
+*/
